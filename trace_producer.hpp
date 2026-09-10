37510658f560e022fc7d8f2484df965ef470c709
leJTA <toulepiarmel@gmail.com>
2026-09-10 07:18:38 +0200

    feat: add trace_producer

---
+++ trace_producer.hpp
@@ -0,0 +1,40 @@
+#ifndef __TRACE_PRODUCER_HPP__
+#define __TRACE_PRODUCER_HPP__
+
+#include <string>
+
+#include "request.hpp"
+
+class Trace_producer {
+public:
+	explicit Trace_producer(const std::string& trace_file)
+		: _trace_file{trace_file}, _request_buffer{nullptr}
+	{}
+
+	void attach_request_buffer(Request_buffer& request_buffer)
+	{
+		_request_buffer = &request_buffer;
+	}
+
+	bool start()
+	{
+		std::ifstream trace{_trace_file};
+		if (!trace.is_open()) {
+			return false;
+		}
+
+		std::string line;
+		while (std::getline(trace, line)) {
+			_request_buffer->push(Request::parse_line(line));
+		}
+		trace.close();
+
+		return true;
+	}
+
+private:
+	std::string _trace_file;
+	Request_buffer* _request_buffer;
+};
+
+#endif // __TRACE_PRODUCER_HPP__
